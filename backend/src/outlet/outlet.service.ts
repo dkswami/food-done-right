5d6f539b7661e3ab042a80d096d0c4070ef9d771
dkswami <dkswami1820@gmail.com>
2023-02-01 16:36:22 +0530

    frontend ui and backend api complteted

--- backend/src/outlet/outlet.service.ts
+++ backend/src/outlet/outlet.service.ts
@@ -20,29 +20,23 @@ export class OutletService {
 	kml = new DOMParser().parseFromString(this.file);
 	KmlAssets = tj.kml(this.kml).features;
 
-
 	async getOutletForLocation(location: string): Promise<any> {
-		const allLocations = await geocoder.geocode(location);
-		const locationCordinates = allLocations.map(({ longitude, latitude }) => {
-			return [longitude, latitude];
+		const locationData = await geocoder.geocode(location);
+		const locationCoordinates = [locationData[0].longitude, locationData[0].latitude];
+
+		const outlet = this.KmlAssets.filter((asset) => {
+			return asset.geometry.type === 'Polygon';
+		}).find((asset) => {
+			const isOutlet = classifyPoint(asset.geometry.coordinates[0], locationCoordinates)	// test coordinate [16.38717,48.20266]
+			console.log('isOutlet', isOutlet)
+			return isOutlet === -1 || isOutlet === 0
 		})
-		let outletName
-		console.log(locationCordinates)
-		this.KmlAssets.forEach((asset) => {
-			if (asset.geometry.type === 'Polygon') {
-				
-				locationCordinates.forEach((location) => {
-					
-					const isOutlet = classifyPoint(asset.geometry.coordinates[0], location)	// test coordinate [16.38717,48.20266]
-					if (isOutlet === -1 || isOutlet === 0) {
-						outletName = asset.properties.name
-					} else {
-						outletName = 'Not found'
-					}
-				})
-			}
-		})
-		return outletName;
+
+		if(outlet) {
+			return outlet.properties.name;
+		} else {
+			return 'No Outlet Found';
+		}
 	}
 
 	getAll(): any {
@@ -51,7 +45,6 @@ export class OutletService {
 
 	async checkOutlet(location: string): Promise<any> {
 		const result = await this.getOutletForLocation(location);
-
 		return result;
 	}
 }
