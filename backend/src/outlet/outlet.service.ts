aea85bc568d562e642bdbd901ccd80486c0bf917
dkswami <dkswami1820@gmail.com>
2023-02-01 12:50:45 +0530

    backend changes

--- backend/src/outlet/outlet.service.ts
+++ backend/src/outlet/outlet.service.ts
@@ -1,25 +1,52 @@
 import { Injectable } from '@nestjs/common';
-import * as parseKML from 'parse-kml';
+import { readFileSync } from 'fs';
+const tj = require('togeojson')
+// node doesn't have xml parsing or a dom. use xmldom
+import { DOMParser } from 'xmldom';
+
 const NodeGeocoder = require('node-geocoder');
+const classifyPoint = require('robust-point-in-polygon');
+
 const options = {
 	provider: 'mapquest',
 	apiKey: 'KoPQHyBHYRZwhIAT94wCMGAM9oU9X8QX'
 }
 const geocoder = NodeGeocoder(options);
-var classifyPoint = require('robust-point-in-polygon');
+
 
 @Injectable()
 export class OutletService {
-	kmlData =  parseKML.toJson('./asset.kml');
+	file = readFileSync('./asset.kml', 'utf-8');
+	kml = new DOMParser().parseFromString(this.file);
+	KmlAssets = tj.kml(this.kml).features;
+
 
 	async getOutletForLocation(location: string): Promise<any> {
-		const result = await geocoder.geocode(location);
-		console.log('location: ', result);
-		return result;
+		const allLocations = await geocoder.geocode(location);
+		const locationCordinates = allLocations.map(({ longitude, latitude }) => {
+			return [longitude, latitude];
+		})
+		let outletName
+		console.log(locationCordinates)
+		this.KmlAssets.forEach((asset) => {
+			if (asset.geometry.type === 'Polygon') {
+				
+				locationCordinates.forEach((location) => {
+					
+					const isOutlet = classifyPoint(asset.geometry.coordinates[0], location)	// test coordinate [16.38717,48.20266]
+					if (isOutlet === -1 || isOutlet === 0) {
+						outletName = asset.properties.name
+					} else {
+						outletName = 'Not found'
+					}
+				})
+			}
+		})
+		return outletName;
 	}
 
-	getAll(): any {		
-		return this.kmlData;
+	getAll(): any {
+		return this.KmlAssets;
 	}
 
 	async checkOutlet(location: string): Promise<any> {
